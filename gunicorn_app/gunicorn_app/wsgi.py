27c2fd9a3b3cc34f928fb1ea17b58168f4c48971
David Daniel <ddavid@tracetechpcb.com>
2024-02-13 07:09:11 +0530

    Renaming BE app to gunicorn_app

--- gunicorn_app/gunicorn_app/wsgi.py
+++ gunicorn_app/gunicorn_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tracetech project.
+WSGI config for gunicorn_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tracetech.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gunicorn_app.settings')
 
 application = get_wsgi_application()
